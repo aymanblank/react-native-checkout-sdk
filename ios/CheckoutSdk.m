5fdd80c83927f9d079f02ea8841760e4dd58f8a0
Ayman Abu Khalifa <Aymanblank@gmail.com>
2019-07-13 00:53:33 +0300

    Added an example view

--- ios/CheckoutSdk.m
+++ ios/CheckoutSdk.m
@@ -1,3 +1,4 @@
+#import "React/RCTViewManager.h"
 @interface RCT_EXTERN_MODULE(CheckoutSdk, RCTViewManager)
 RCT_EXPORT_VIEW_PROPERTY(isOn, BOOL)
 RCT_EXPORT_VIEW_PROPERTY(onStatusChange, RCTDirectEventBlock)
